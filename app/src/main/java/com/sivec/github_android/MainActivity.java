c2ed856705033c626e4105c3feaca2e7122bc169
Andraz Sivec <4easivec@dijak.zavod>
2018-01-18 15:23:36 +0100

    Initial commit

---
+++ app/src/main/java/com/sivec/github_android/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sivec.github_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
